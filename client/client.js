504acabda53e026cdea660172afd1beb40fc2a56
dcruzships <bvd5889@rit.edu>
2020-03-02 23:26:12 -0500

    Added more functionality
    Oof

--- client/client.js
+++ client/client.js
@@ -6,6 +6,8 @@ const newMsgForm = document.querySelector("#newMsgForm");
 const myMsgsButt = document.querySelector('#myMsgs');
 const allMsgsButt = document.querySelector('#allMsgs');
 const sortButton = document.querySelector('#sortSelectButton');
+let whatMessages;
+let gridCount = 1;
 
 // initalizes page
 const init = () =>
@@ -25,6 +27,67 @@ const init = () =>
   newMsgForm.addEventListener('submit', sendMessage);
   myMsgsButt.addEventListener('click', getMyMessages);
   allMsgsButt.addEventListener('click', getAllMessages);
+};
+
+newMsgButt.onclick = () => {
+  popup.style.display = "block";
+  // clear the content inside the modal
+  newMsgForm.reset();
+};
+
+closeButt.onclick = () => {
+  popup.style.display = "none";
+};
+
+window.onclick = e => {
+  if (e.target == popup) {
+    popup.style.display = "none";
+  }
+};
+
+sendMsgButt.onclick = () => {
+  popup.style.display = "none";
+};
+
+// display all messages from returned JSON
+const showMsgs = (msgArray, content) =>
+{
+  //clear the Content
+  content.innerHTML = "";
+
+  //grid always starts with a row and a plus button to add new messages
+  content.innerHTML += `<div class='grid'><div class='row'><div class='box'><div class='inner' id='newMsgButton'>+</div></div>`;
+
+  msgArray.forEach((msg, =>
+  {
+    content.innerHTML += `<div class='box'><div class='inner'>`;
+
+    //add date, body, and author
+    content.innerHTML += `<h2>${msg.date}, ${msg.time}</h2>`;
+    content.innerHTML += `<p>${msg.message}</p>`;
+    content.innerHTML += `<h2>From: ${msg.name}`;
+
+    //close the message div
+    content.innerHTML += `</div></div>`;
+
+    gridCount++;
+
+    // if grid row is full, add another row
+    if(gridCount > 4)
+    {
+     content.innerHTML += `</div>`;
+     content.innerHTML += `<div class='row'>`;
+    }
+  });
+
+  //if the grid was not full, close it anyways
+  if(gridCount < 5)
+  {
+    content.innerHTML += `</div>`;
+  }
+
+  //close the grid and content
+  content.innerHTML += `</div></div>`;
 };
 
 //function to parse our response
@@ -55,20 +118,26 @@ const handleResponse = (xhr) => {
 
   //check the status code
   switch(xhr.status) {
-    case 200: //success
+    case 200: // success
       content.innerHTML = `<b>Success</b>`;
       break;
-    case 201: //created
+    case 201: // created
       content.innerHTML = '<b>Create</b>';
       break;
-    case 204: //updated (no response back from server)
+    case 204: // updated (no response back from server)
       content.innerHTML = '<b>Updated (No Content)</b>';
-      return;
-    case 400: //bad request
-      content.innerHTML = `<b>Bad Request</b>`;
       break;
-    default: //any other status code
-      content.innerHTML = `Error code not implemented by client.`;
+    case 400: // not all fields filled out
+      alert("All fields are required");
+      break;
+    case 404: // updated (no response back from server)
+      alert("Resource not found");
+      break;
+    case 500: // server crashes
+      alert("Internal Server Error");
+      break;
+    default: // any other status code
+      alert("Error: Code not implemented by client");
       break;
   }
 
@@ -77,49 +146,67 @@ const handleResponse = (xhr) => {
 };
 
 //function to send our post request
-const sendPost = (e, nameForm) => {
-  //prevent the browser's default action (to send the form on its own)
-  e.preventDefault();
+const sendPost = (e, msgForm) =>
+{
+  const msgAction = newMsgForm.getAttribute('action');
+  const msgMethod = newMsgForm.getAttribute('method');
 
-  //grab the forms action (url to go to)
-  //and method (HTTP method - POST in this case)
-  const nameAction = nameForm.getAttribute('action');
-  const nameMethod = nameForm.getAttribute('method');
+  // grabs the fields from the form
+  const nameField = newMsgForm.querySelector('#nameField');
+  const msgField = newMsgForm.querySelector('#msgField');
 
-  //grab the form's name and age fields so we can check user input
-  const nameField = nameForm.querySelector('#nameField');
-  const ageField = nameForm.querySelector('#ageField');
+  // get date and time of message send
+  const date = "";
+  const time = "";
 
-  //create a new Ajax request (remember this is asynchronous)
   const xhr = new XMLHttpRequest();
-  //set the method (POST) and url (action field from form)
-  xhr.open(nameMethod, nameAction);
+  xhr.open(msgMethod, msgAction);
 
-  //set our request type to x-www-form-urlencoded
-  //which is one of the common types of form data.
-  //This type has the same format as query strings key=value&key2=value2
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  //set our requested response type in hopes of a JSON response
   xhr.setRequestHeader ('Accept', 'application/json');
 
-  //set our function to handle the response
   xhr.onload = () => handleResponse(xhr);
 
-  //build our x-www-form-urlencoded format. Without ajax the
-  //browser would do this automatically but it forcefully changes pages
-  //which we don't want.
-  //The format is the same as query strings, so key=value&key2=value2
-  //The 'name' fields from the inputs are the variable names sent to
-  //the server.
-  //So ours might look like  name=test&age=22
-  //Again the 'name' fields in the form are the variable names in the string
-  //and the variable names the server will look for.
-  const formData = `name=${nameField.value}&age=${ageField.value}`;
+  // adds values put in by user to formData object
+  const formData = `name=${nameField.value}&msg=${msgField.value}&date=${date}&time=${time}`;
 
-  //send our request with the data
+  // sends object
   xhr.send(formData);
+  console.dir(`form data: ${formData}`);
 
-  //return false to prevent the browser from trying to change page
+  // don't reload the page
+  e.preventDefault();
+
+  // resets the fields
+  nameField.value = "";
+  msgField.value = "";
+
+  // alerts the user that the message was sent and added to the wall
+  alert('Message sent!');
+
+  // so we can continue
+  return false;
+};
+
+// updates the page
+const requestUpdate = (e, method) =>
+{
+  // hides the form, should already be hidden but just in case lol
+  popup.style.display = "none";
+
+  // creates new request
+  const xhr = new XMLHttpRequest();
+  xhr.open('get', method);
+
+  xhr.setRequestHeader('Accept', 'application/json');
+  if(method == 'get') {
+    xhr.onload = () => handleResponse(xhr, true);
+  } else {
+    xhr.onload = () => handleResponse(xhr, false);
+  }
+  xhr.send();
+
+  e.preventDefault();
   return false;
 };
 
